34b5f20d35b96711e9c0f248ec7dc7b1a3d4aaca
niksh211 <niksh2511@gmail.com.com>
2023-12-07 22:36:28 +0530

    added all

--- main.js
+++ main.js
@@ -1,38 +1,80 @@
 $(document).ready(async function () {
+  const destinationData = await fetch("./destination.json").then((response) =>
+    response.json()
+  );
+  const sourceData = await fetch("./source.json").then((response) =>
+    response.json()
+  );
+  let flightsData = await fetch("./flights.json").then((response) =>
+    response.json()
+  );
 
-    const destinationData = await fetch('./destination.json').then((response) => response.json().then((data) => data))
+  console.log(flightsData)
 
-    const sourceData = await fetch('./source.json').then((response) => response.json().then((data) => data))
+  function userBooking() {
+    const container = $("#selection");
 
-    const flightsData = await fetch('./flights.json').then((response) => response.json().then((data) => data))
+    // destination dropdown
+    const destinationDropdown = $("#Destination");
+    destinationData.forEach((des) => {
+      const option = $("<option>");
+      option.val(des.id);
+      option.text(des.name);
+      destinationDropdown.append(option);
+    });
+
+    // source dropdown
+    const sourceDropdown = $("#Source");
+    sourceData.forEach((source) => {
+      const option = $("<option>");
+      option.val(source.id);
+      option.text(source.name);
+      sourceDropdown.append(option);
+    });
+  }
+
+  // Function to search for flights
+  function searchFlights() {
+    const sourceId = $("#Source").val();
+    const destinationId = $("#Destination").val();
+    const searchInput = $("#searchInput").val().toLowerCase();
+
+    // Filter flights based on source, destination, and search input
+    const matchingFlights = flightsData.filter(flight =>
+        flight.sourceID == sourceId && flight.destinationID == destinationId &&
+        (flight.name.trim().toLowerCase().includes(searchInput) ||
+         flight.departureDate.includes(searchInput))
+    );
+    
+    // Display the matching flights
+    displayFlights(matchingFlights);
+  }
 
 
-    function userBooking() {
-        const container = $("#selection");
+  // Function to display flights
+function displayFlights(flights) {
+    const flightResultsContainer = $("#searchResults");
+    flightResultsContainer.empty(); // Clear previous results
 
-        // destination dropdown
-        const destinationDropdown = $("#Destination")
-        destinationData.forEach(des => {
-            const option = $("<option>");
-            option.val(des.id);
-            option.text(des.name);
-            destinationDropdown.append(option)
-        });
-
-        // source dropdown
-        const sourceDropdown = $("#Source")
-        sourceData.forEach(source => {
-            const option = $("<option>");
-            option.val(source.id);
-            option.text(source.name)
-            sourceDropdown.append(option)
-        })
-
-        
-
+    if (flights.length === 0) {
+        flightResultsContainer.append('<p>No flights found for the selected route.</p>');
+        return;
     }
 
+    const resultList = $("<ul>");
+    flights.forEach(flight => {
+        const listItem = $("<li>");
+        listItem.text(`${flight.name} - Price: $${flight.price} - Departure Date: ${flight.departureDate}`);
+        resultList.append(listItem);
+    });
+
+    flightResultsContainer.append(resultList);
+}
 
 
-    userBooking()
-})
+  $("#searchButton").on("click", function () {
+    searchFlights();
+  });
+
+  userBooking();
+});
